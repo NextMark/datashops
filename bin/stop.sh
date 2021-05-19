10ddb2dda8e2e1a23e0340e1ee7341dcb765703e
NextMark <qinshiweiy@gmail.com>
2021-05-19 20:18:54 +0800

    fix

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,1 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ $# -le 0 ]; then
+  echo "special stop service name."
+  exit 1
+fi
+server=$1
+kill $(cat pid/datashops-${server}.pid)
+echo "datashops-${server} is shutting down"
